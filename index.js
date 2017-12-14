1de5460934cb93068fa4d2abfe07253303b52b6c
Carter Capocaccia <Carter.Capocaccia@Gmail.com>
2017-12-14 09:45:59 -0600

    adds js to no longer rely on cornify.  Requesting HTTP over HTTPS sucks

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-let cornify = require('cornified')
-
 let pressed = [];
 const code = 'ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightbaEnter';
 window.addEventListener('keyup', (e) => {
 	pressed.push(e.key);
 	pressed.splice(-code.length - 1, pressed.length - code.length);
 	if(pressed.join('') === code){
-		cornify_add();	
+		var body = document.querySelector('body');
+		body.style.transform = "rotate(10800deg)";
+		body.style.transition = 'transform 5s';
+		setTimeout(changeCodeText, 5250);
 	}
 });
+
+function changeCodeText(){
+	var codeContainer = document.querySelector('.code_container');
+	codeContainer.innerHTML = 'Thanks for playing! Refresh the page to try it again!'
+}
